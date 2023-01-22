ce1bb4a598df1165b75b53dad9a42870a0e434ad
Armin Mehraeen <armin.mehraeen@gmail.com>
2023-01-22 15:44:57 +0330

    update

--- lib/gpassword.dart
+++ lib/gpassword.dart
@@ -3,21 +3,23 @@ library gpassword;
 import 'dart:math';
 
 class GPassword {
-  List<String> generate({int passLen = 16}) {
-    List<String> passwords = [];
-    List.generate(9, (index) => passwords.add(_compute(passLen: passLen)));
-    return passwords;
-  }
+  static const _passwordLength = 16;
+  static const _passwordCount = 9;
+  static const String _upperWords = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  static const String _lowerWords = 'abcdefghijklmnopqrstuvwxyz';
+  static const String _numbers = '1234567890';
+  static const String _symbols = '!@#&';
 
-  String _compute({required int passLen}) {
-    String upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    String lower = 'abcdefghijklmnopqrstuvwxyz';
-    String numbers = '1234567890';
-    String symbols = '!@#&';
-    int passLength = passLen;
-    String seed = upper + lower + numbers + symbols;
+  List<String> generate({int passwordLength = _passwordLength}) =>
+      List.generate(
+              _passwordCount, (index) => _compute(passLength: passwordLength))
+          .toList();
+
+  String get _concat => _upperWords + _lowerWords + _numbers + _symbols;
+
+  String _compute({required int passLength}) {
     String password = '';
-    List<String> list = seed.split('').toList();
+    List<String> list = _concat.split('').toList();
     Random rand = Random();
     for (int i = 0; i < passLength; i++) {
       int index = rand.nextInt(list.length);
